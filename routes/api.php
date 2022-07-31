6d8f6cb174b7321c00d78c10b8807ae0c76bc7ed
arybobysiregar <aryboby1@gmail.com>
2022-07-31 22:58:24 +0700

    Add authController API with sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CaseController;
 use App\Http\Controllers\API\DiseaseController;
 use App\Http\Controllers\API\SolutionController;
@@ -22,22 +23,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//disease API
-Route::post('disease',[DiseaseController::class,'add']);
-Route::get('disease',[DiseaseController::class,'all']);
-//sympthon API
-Route::post('sympthon',[SympthonController::class,'add']);
-Route::get('sympthon',[SympthonController::class,'all']);
-//case API
-Route::post('case',[CaseController::class,'add']);
-Route::get('case',[CaseController::class,'all']);
-//case pivot
-Route::post('casePivot',[CaseController::class,'addCasePivot']);
-Route::get('casePivot',[CaseController::class,'allCasePivot']);
-//solution
-Route::post('solution',[SolutionController::class,'add']);
-Route::get('solution',[SolutionController::class,'all']);
-Route::post('solutionPivot',[SolutionController::class,'addSolutionPivot']);
-Route::get('solutionPivot',[SolutionController::class,'allSolutionPivot']);
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('logout',[AuthController::class,'logout']);
 
+    //disease API
+    Route::post('disease',[DiseaseController::class,'add']);
+    Route::get('disease',[DiseaseController::class,'all']);
+    //sympthon API
+    Route::post('sympthon',[SympthonController::class,'add']);
+    Route::get('sympthon',[SympthonController::class,'all']);
+    //case API
+    Route::post('case',[CaseController::class,'add']);
+    Route::get('case',[CaseController::class,'all']);
+    //case pivot
+    Route::post('casePivot',[CaseController::class,'addCasePivot']);
+    Route::get('casePivot',[CaseController::class,'allCasePivot']);
+    //solution
+    Route::post('solution',[SolutionController::class,'add']);
+    Route::get('solution',[SolutionController::class,'all']);
+    Route::post('solutionPivot',[SolutionController::class,'addSolutionPivot']);
+    Route::get('solutionPivot',[SolutionController::class,'allSolutionPivot']);
+});
 
+Route::post('register',[AuthController::class,'register']);
+Route::post('login',[AuthController::class,'login']);
